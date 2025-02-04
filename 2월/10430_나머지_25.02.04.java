6b7e2bcfe6ae25bac878be07dd9b5ae53febff4d
nnijgnus <76596376+k000927@users.noreply.github.com>
2025-02-05 01:56:12 +0900

    [10430 나머지 - 25.02.04]

--- 2월/10430_나머지_25.02.04.java
+++ 2월/10430_나머지_25.02.04.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 10430_나머지_25.02.01 {
+public class 10430_나머지_25.02.04 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
