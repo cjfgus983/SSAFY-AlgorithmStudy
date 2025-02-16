ee16de97a43264fccd09db765c639f653568a376
yurichoi0510 <chldbfl159@naver.com>
2025-02-16 20:45:52 +0900

    [15652 N과 M (4) - 25.02.16]

---
+++ 2월 2주차/YuRi/Yuri_15652/Main.java
@@ -0,0 +1,56 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] arr;
+	//static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		//visited = new boolean[N+1];
+		arr= new int[M];
+		
+		nm(N,M, 0,1);
+		
+		System.out.println(sb);
+		
+	}
+	
+	public static void nm(int n, int m, int now,int start) {
+		if(m==now) {
+			for(int a :arr) {
+				sb.append(a+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start;i<=n;i++) {
+			/*
+			if(!visited[i]) {
+				visited[i] =true;
+				arr[now] = i;
+				nm(n,m,now+1);
+				visited[i] =false;
+			}
+			*/
+			
+			arr[now] = i;
+			nm(n,m,now+1,i);
+		}
+		
+		return;
+	}
+
+}
