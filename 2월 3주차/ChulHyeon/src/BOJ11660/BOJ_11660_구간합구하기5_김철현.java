44fbad437bb04810575357faec7b98576529318e
munncoding <cjfgus983@gmail.com>
2025-02-20 23:03:59 +0900

    Update BOJ_11660_구간합구하기5_김철현.java

--- 2월 3주차/ChulHyeon/src/BOJ11660/BOJ_11660_구간합구하기5_김철현.java
+++ 2월 3주차/ChulHyeon/src/BOJ11660/BOJ_11660_구간합구하기5_김철현.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class BOJ_11660_구간합구하기5_김철현 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
