5ae390c3e05652d9b78adc66e2edae552ed8bc64
김은재 <rlaqkaql1012@naver.com>
2025-02-22 22:43:16 +0900

    [14501 퇴사하고싶다 - 25.02.21]

--- 2월 3주차/BOJ14501/Main.java
+++ 2월 3주차/BOJ14501/Main.java
@@ -1,6 +1,6 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
+package BOJ14501;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 public class Main {
 	static int[][] talk;
