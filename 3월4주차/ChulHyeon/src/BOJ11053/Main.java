8491abf22fd2385faf3fd2d597c76b0a48535dd6
munncoding <cjfgus983@gmail.com>
2025-03-25 19:24:20 +0900

    [11053 가장긴증가하는부분수열 - 25.03.24]

---
+++ 3월4주차/ChulHyeon/src/BOJ11053/Main.java
@@ -0,0 +1,28 @@
+package BOJ11053;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine().trim());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        st = new StringTokenizer(br.readLine().trim());
+        for(int i=0;i < n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = 1;
+        // 인덱스 2번부터 이전 인덱스를 뒤지며 현재 자기 위치보다 작은걸 찾아서 dp 갱신
+        for(int i=1;i >=0 ; i--)
+        {
+
+        }
+    }
+}
